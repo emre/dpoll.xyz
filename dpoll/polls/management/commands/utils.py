b02a3cb9155ca06a58ca8ca797b798d23a92faeb
Emre Yilmaz <mail@emreyilmaz.me>
2018-11-16 13:09:55 +0300

    Update comment body on upvote

--- dpoll/polls/management/commands/utils.py
+++ dpoll/polls/management/commands/utils.py
@@ -3,6 +3,8 @@ def get_comment_body(curator):
 Thanks for contributing to the dPoll content.
 
 You have been upvoted from our community curation account (@dpoll.curation) in courtesy of {curator}.
+
+Come, join our community at [dPoll discord server](https://discord.gg/ZcV8SGr).
 ***
 <sup>If you want to support dPoll curation, you can also delegate some steem power. Quick steem connect links to delegate: 
 [50SP](https://steemconnect.com/sign/delegateVestingShares?delegatee=dpoll.curation&vesting_shares=50%20SP) | [100SP](https://steemconnect.com/sign/delegateVestingShares?delegatee=dpoll.curation&vesting_shares=100%20SP) | [250SP](https://steemconnect.com/sign/delegateVestingShares?delegatee=dpoll.curation&vesting_shares=250%20SP) | [500SP](https://steemconnect.com/sign/delegateVestingShares?delegatee=dpoll.curation&vesting_shares=500%20SP)  
