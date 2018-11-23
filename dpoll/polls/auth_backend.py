9d17f80c39add89f86c6bb7d3984a1b4f48c57d4
Emre Yilmaz <mail@emreyilmaz.me>
2018-11-23 23:50:08 +0300

    We don't store tokens

--- dpoll/polls/auth_backend.py
+++ dpoll/polls/auth_backend.py
@@ -19,12 +19,10 @@ class SteemConnectBackend:
         user_model = get_user_model()
         try:
             user_instance = user_model.objects.get(username=user["name"])
-            user_instance.token = kwargs.get("access_token")
             user_instance.save()
         except user_model.DoesNotExist:
             user_instance = user_model.objects.create_user(
-                username=user["name"],
-                token=kwargs.get("access_token"))
+                username=user["name"])
         return user_instance
 
     def get_user(self, user_id):
