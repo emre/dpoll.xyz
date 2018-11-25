1ad2f5054c9c198008a3e8f649366ee547fa9603
Emre Yilmaz <mail@emreyilmaz.me>
2018-11-25 21:40:29 +0300

    Vitalize the edit view

--- dpoll/polls/urls.py
+++ dpoll/polls/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('user/@<str:user>/', views.profile, name='profile'),
     path('create/', views.create_poll, name='create-poll'),
     path('team/', views.team, name='team'),
+    path('edit/@<str:author>/<str:permlink>/', views.edit_poll, name='edit'),
     path('polls_by_vote/', views.polls_by_vote_count, name='polls-by-vote'),
 ]
