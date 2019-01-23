e0bab6511d2356cf44849196a87f8ad2b26205f3
Emre Yilmaz <mail@emreyilmaz.me>
2019-01-23 21:28:21 +0300

    Add a new field to set feature flag on multiple choice selection

--- dpoll/polls/models.py
+++ dpoll/polls/models.py
@@ -45,6 +45,7 @@ class Question(models.Model):
     username = models.CharField(max_length=255)
     permlink = models.CharField(max_length=255, blank=True, null=True,
                                 db_index=True)
+    allow_multiple_choices = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text
