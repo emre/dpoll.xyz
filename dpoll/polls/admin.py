93cd601c561bb5e979b1ffe2cd32daa278faca60
Emre Yilmaz <mail@emreyilmaz.me>
2019-02-05 13:24:17 +0300

    Store vote transaction references between dPoll and blockchain

--- dpoll/polls/admin.py
+++ dpoll/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Question, Choice, PromotionTransaction
+from .models import User, Question, Choice, PromotionTransaction, VoteAudit
 from django.contrib.auth.admin import UserAdmin
 
 
@@ -13,7 +13,17 @@ class CustomUserAdmin(UserAdmin):
     )
 
 
+class ChoicesInline(admin.TabularInline):
+    model = VoteAudit.choices.through
+    extra = 0
+
+
+class VoteAuditAdmin(admin.ModelAdmin):
+    inlines = (ChoicesInline, )
+    exclude = ('choices', )
+
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(PromotionTransaction)
+admin.site.register(VoteAudit, VoteAuditAdmin)
