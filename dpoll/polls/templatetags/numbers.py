7915c596285a5b9357d2190e6d9021838238090f
Emre Yilmaz <mail@emreyilmaz.me>
2019-02-10 16:55:31 +0300

    humanized format on SPs

---
+++ dpoll/polls/templatetags/numbers.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def cool_number(value, num_decimals=2):
+    """
+    Django template filter to convert regular numbers to a
+    cool format (ie: 2K, 434.4K, 33M...)
+    :param value: number
+    :param num_decimals: Number of decimal digits
+    """
+
+    int_value = int(value)
+    formatted_number = '{{:.{}f}}'.format(num_decimals)
+    if int_value < 1000:
+        return str(int_value)
+    elif int_value < 1000000:
+        return formatted_number.format(int_value/1000.0).rstrip('0.') + 'K'
+    else:
+        return formatted_number.format(int_value/1000000.0).rstrip('0.') + 'M'
