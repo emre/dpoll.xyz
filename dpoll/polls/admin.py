89ebc787dfa1aa170579f1c97d693d06d6632514
Emre Yilmaz <mail@emreyilmaz.me>
2019-04-08 14:20:28 +0200

    Add search support to the user admin

--- dpoll/polls/admin.py
+++ dpoll/polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import User, Question, Choice, PromotionTransaction, VoteAudit
+from django.contrib.auth.admin import UserAdmin
+
+
+class MyUserAdmin(UserAdmin):
+    search_fields = ('username', )
 
 
 class ChoicesInline(admin.TabularInline):
@@ -12,7 +17,7 @@ class VoteAuditAdmin(admin.ModelAdmin):
     exclude = ('choices', )
 
 
-admin.site.register(User)
+admin.site.register(User, MyUserAdmin)
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(PromotionTransaction)
